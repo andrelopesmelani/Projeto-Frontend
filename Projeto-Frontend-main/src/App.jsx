9f9f5a00541c1400cd88245551bc79809d390911
andrelopesmelani <andre.lopes.melani@gmail.com>
2024-11-11 23:45:49 -0300

    feature: CRUD de Eventos

--- Projeto-Frontend-main/src/App.jsx
+++ Projeto-Frontend-main/src/App.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import Layout from "./components/Layout";
 import Ingressos from "./pages/Ingressos";
 import Locais from "./pages/Locais";
-// import Eventos from "./pages/Eventos";
+import Eventos from "./pages/Eventos";
 // import Participantes from "./pages/Participantes";
 
 
@@ -15,8 +15,8 @@ function App() {
         <Route path="/" element={<Layout />} />
         <Route path="/ingressos" element={<Layout><Ingressos /></Layout>} />
         <Route path="/locais" element={<Layout><Locais /></Layout>} />
-        {/* <Route path="/eventos" element={<Layout><Eventos /></Layout>} />
-        <Route path="/participantes" element={<Layout><Participantes /></Layout>} /> */}
+        <Route path="/eventos" element={<Layout><Eventos /></Layout>} />
+      {/*<Route path="/participantes" element={<Layout><Participantes /></Layout>} /> */}
       </Routes>
     </>
   );
